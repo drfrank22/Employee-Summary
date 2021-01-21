ab823551f27d7127ddb695c9e88b0ae27285ada9
Dylan Frank <drfrank22@gmail.com>
2021-01-21 01:11:13 -0500

    app js close to finish

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ function startApp() {
             {
                 type: "input",
                 name: "managerName",
-                message: "What is your manager's name?"
+                message: "What is the manager's name?"
             },
             {
                 type: "input",
                 name: "managerEmail",
-                message: "What is your manager's e-mail address?"
+                message: "What is the manager's e-mail address?"
             },
             {
                 type: "list",
                 name: "managerId",
-                message: "What is your manager's ID Badge Number?",
+                message: "What is the manager's ID Badge Number?",
                 choices: [
                     "1",
                     "2",
@@ -42,7 +42,7 @@ function startApp() {
             {
                 type: "list",
                 name: "managerOffice",
-                message: "What is your manager's office number?",
+                message: "What is the manager's office number?",
                 choices: [
                     "502",
                     "402",
@@ -85,7 +85,79 @@ function startApp() {
         });
     };
 
-    
+    function addEngineer() {
+        console.log("Let's add an Engineer to your team!!")
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineer's name?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the engineer's e-mail address?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the engineer's ID Badge Number?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is the engineer's Github Username?"
+            }
+        ]).then(responses => {
+            const engineer = new Engineer(responses.engineerName, responses.engineerEmail,
+                responses.engineerId, responses.engineerGithub);
+            teamPersonnel.push(engineer);
+            allId.push(responses.engineerId);
+            addTeamMembers();
+        });
+    };
+
+    function addIntern() {
+        console.log("Let's add an Intern to your team!!")
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the intern's name?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's e-mail address?"
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is the intern's ID Badge Number?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What university or college does the intern attend?"
+            }
+        ]).then(responses => {
+            const intern = new Intern(responses.internName, responses.internEmail,
+                responses.internId, responses.internSchool);
+            teamPersonnel.push(intern);
+            allId.push(responses.internId);
+            addTeamMembers();
+        });
+    };
+
+    function createTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamPersonnel), "utf-8");
+    };
+
+    addManager();
+
 };
 
 startApp();
