fb5782303c9a9d2d6ab89d763c3e19a5c87d924b
Dylan Frank <drfrank22@gmail.com>
2021-01-21 00:56:23 -0500

    adding prompts to app js

--- app.js
+++ app.js
@@ -10,26 +10,82 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamPersonnel = [];
+const allId = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function startApp() {
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    function addManager() {
+        console.log("Let's start building your team!!")
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's e-mail address?"
+            },
+            {
+                type: "list",
+                name: "managerId",
+                message: "What is your manager's ID Badge Number?",
+                choices: [
+                    "1",
+                    "2",
+                    "3",
+                    "4"
+                ]
+            },
+            {
+                type: "list",
+                name: "managerOffice",
+                message: "What is your manager's office number?",
+                choices: [
+                    "502",
+                    "402",
+                    "302",
+                    "202"
+                ]
+            }
+        ]).then(responses => {
+            const manager = new Manager(responses.managerName, responses.managerEmail,
+                responses.managerId, responses.managerOffice);
+            teamPersonnel.push(manager);
+            allId.push(responses.managerId);
+            addTeamMembers();
+        });
+    };
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    function addTeamMembers() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "teamMemberChoice",
+                message: "What team member position would you like to add?",
+                choices: [
+                    "Intern.",
+                    "Engineer.",
+                    "I am done building my team."
+                ]
+            }
+        ]).then(teamBuilderChoice => {
+            switch(teamBuilderChoice.teamMemberChoice) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    createTeam();
+            }
+        });
+    };
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    
+};
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+startApp();
